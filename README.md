bfd431ccaaa4b26e12619a5e1c2b6aaf21a33811
pairing4good <pairing4good@gmail.com>
2022-11-03 09:37:23 -0500

    updated tests and readme

--- README.md
+++ README.md
@@ -1,10 +1,18 @@
-# 025 Cascading Styles
+# 026 CSS Variables
 
-[![025 Cascading Styles](https://img.youtube.com/vi/LwDAhWq7hFc/0.jpg)](https://www.youtube.com/watch?v=LwDAhWq7hFc)
+[![026 CSS Variables](https://img.youtube.com/vi/eRH9aMZk6f8/0.jpg)](https://www.youtube.com/watch?v=eRH9aMZk6f8)
 
 The `index.html` file
-- should add styling so that the paragraph color purple overrides the text-area color of hot pink
+- should extract 200px into a css variable named `wide-text-size`
+- should extract 150px into a css variable named `narrow-text-size`
+- should extract 10px into a variable named text-padding
+- should override the `narrow-text-size` css variable in the `narrow-text` css class definition scope with `120px`
+- should set the `wide-text-size` css variable to `180px` when the screen is `150px` wide or less
 
 Resources:
-- https://www.w3schools.com/css/css_important.asp
-- https://www.w3schools.com/css/css_specificity.asp
+- https://www.w3schools.com/css/css3_variables.asp
+- https://www.w3schools.com/css/css3_variables_overriding.asp
+- https://www.w3schools.com/cssref/css3_pr_mediaquery.asp
+
+Building toward CSTA Standards:
+- Decompose problems into smaller components through systematic analysis, using constructs such as procedures, modules, and/or objects. (3A-AP-17) https://www.csteachers.org/page/standards
--- test/verify.test.js
+++ test/verify.test.js
@@ -37,13 +37,70 @@ afterEach(async () => {
   await browser.close();
 });
 
-describe('the paragraph color purple', () => {
-  it('should override the text-area color', async () => {
-    const color = await page.$eval('p', (paragraph) => {
-      let style = window.getComputedStyle(paragraph);
-      return style.getPropertyValue('color');
-    });
+describe('the value 200px', () => {
+    it('should be contained in a variable named wide-text-size', async () => {
+      const variableDefinitionCount = await page.$eval('style', (style) => {
+        return style.innerHTML.match(/--wide-text-size.*:.*200px/g).length;
+      });
       
-    expect(color).toBe('rgb(128, 0, 128)');
-  });
+      expect(variableDefinitionCount).toEqual(1);
+        
+      const variableUsageCount = await page.$eval('style', (style) => {
+        return style.innerHTML.match(/var.*\(.*--wide-text-size.*\)/g).length;
+      });
+        
+      expect(variableUsageCount).toEqual(1);
+    });
+});
+
+describe('the value 150px', () => {
+    it('should be contained in a variable named narrow-text-size', async () => {
+      const variableDefinitionCount = await page.$eval('style', (style) => {
+        return style.innerHTML.match(/--narrow-text-size.*:.*150px/g).length;
+      });
+        
+      expect(variableDefinitionCount).toEqual(1);
+        
+      const variableUsageCount = await page.$eval('style', (style) => {
+        return style.innerHTML.match(/var.*\(.*--narrow-text-size.*\)/g).length;
+      });
+        
+      expect(variableUsageCount).toEqual(1);
+    });
+});
+
+describe('the variable narrow-text-size', () => {
+    it('should be overridden for the narrow-text class definition with the value 120px', async () => {
+      const variableDefinitionCount = await page.$eval('style', (style) => {
+        return style.innerHTML.match(/--narrow-text-size.*:.*120px/g).length;
+      });
+        
+      expect(variableDefinitionCount).toEqual(1);
+    });
+});
+
+describe('the value 10px', () => {
+    it('should be contained in a variable named text-padding', async () => {
+      const variableDefinitionCount = await page.$eval('style', (style) => {
+        return style.innerHTML.match(/--text-padding.*:.*10px/g).length;
+      });
+
+      expect(variableDefinitionCount).toEqual(1);
+        
+      const variableUsageCount = await page.$eval('style', (style) => {
+        return style.innerHTML.match(/var.*\(.*--text-padding.*\)/g).length;
+      });
+        
+      expect(variableUsageCount).toEqual(2);
+    });
+});
+
+describe('the wide-text-size variable', () => {
+    it('should be set to 180px when the screen is 150px wide or less', async () => {
+      const variableDefinitionCount = await page.$eval('style', (style) => {
+        return style.innerHTML.match(/@media.*\(.*max-width.*:.*150px.*\).*{[\s\S][^}]*--wide-text-size.*:.*180px;/g).length;
+      });
+        
+      expect(variableDefinitionCount).toEqual(1);
+    });
 });
